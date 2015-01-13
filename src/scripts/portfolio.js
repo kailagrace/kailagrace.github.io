6e0d0eebf95985f98431cff29b493f1109e92138
Kaila Grace <kaila.manca@gmail.com>
2015-01-13 18:39:29 -0500

    added show/hide functionality

--- src/scripts/portfolio.js
+++ src/scripts/portfolio.js
@@ -21,6 +21,23 @@
     }
 }());
 
+
+
+//show and hide content script//
+function showHide(shID) {
+   if (document.getElementById(shID)) {
+      if (document.getElementById(shID+'-show').style.display != 'none') {
+         document.getElementById(shID+'-show').style.display = 'none';
+         document.getElementById(shID).style.display = 'block';
+      }
+      else {
+         document.getElementById(shID+'-show').style.display = 'inline';
+         document.getElementById(shID).style.display = 'none';
+      }
+   }
+}
+
+
 // Place any jQuery/helper plugins in here.
 
 
