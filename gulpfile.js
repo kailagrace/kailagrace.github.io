3f51b4264f74e957e40bf1e34c6df0f6cbda5316
Kaila Grace <kaila.manca@gmail.com>
2015-01-20 23:00:35 -0500

    compressed images

--- gulpfile.js
+++ gulpfile.js
@@ -71,15 +71,7 @@ gulp.task('clean', function () {
 });
 
 
-//gulp-sass
-var gulp = require('gulp');
-var sass = require('gulp-sass');
 
-gulp.task('sass', function () {
-    gulp.src('./scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./css'));
-});
 
 
 //livereload
